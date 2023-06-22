7520db2e5f846cf52d017dd9e45b7054a44471a6
이지원 <manz19@ijiwon-ui-MacBookAir.local>
2023-06-22 18:48:31 +0900

    ADD : MainComponent 수정

--- Frontend/git-test/src/Router.js
+++ Frontend/git-test/src/Router.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Login from './pages/Login/Login';
+import Main from './pages/Login/Main/Main';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Main />} />
+       
+        
       </Routes>
     </BrowserRouter>
   );
