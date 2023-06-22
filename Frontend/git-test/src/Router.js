36251f452a8d087a6beefc2e0277ff742f711fe7
이지원 <manz19@ijiwon-ui-MacBookAir.local>
2023-06-22 20:34:13 +0900

    Conflict 해결

--- Frontend/git-test/src/Router.js
+++ Frontend/git-test/src/Router.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Login from './pages/Login/Login';
-
+import Main from './pages/Login/Main/Main';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />    
+      <Route path="/" element={<App />} />
+       
+        
       </Routes>
     </BrowserRouter>
   );
